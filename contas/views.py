1b34f2331f1009c5a629a06d6a1bfbdedd66e042
willebabi <will.wag@gmail.com>
2020-06-23 22:23:42 -0300

    Adicionando tabelas e views

--- contas/views.py
+++ contas/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-
+from .models import Transacao
 # Create your views here.
 from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['trans'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
     # return HttpResponse(html)
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacao'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)    
